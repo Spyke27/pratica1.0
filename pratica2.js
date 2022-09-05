1cef634583404fcc275d5b95fafa390129e804d8
Spyke27 <rodrigoeufrasio2@gmail.com>
2022-09-05 10:31:38 -0300

    quinto commit - imagem

--- pratica2.js
+++ pratica2.js
@@ -3,3 +3,24 @@
 Bem vindo à segunda pratica de GIT
 
 */
+
+// git init - inicia um repositorio na minha maquina 
+
+//git add . - adiciona todos os arquivos à minha pasta escolhida
+
+//git add "arquivos específicos.extensão" - envia arquivos específicos para o meu repositorio
+
+//git remote add origin (link do repositorio) - conecta meu repositorio local até o meu repositorio no Github (nuvem)
+
+//git push - envia os arquivos commitados até o Github
+
+//git pull - puxa os arquivos do Github para a minha maquina
+
+//git commit -m "meu commit" - prepara o arquivo para ser enviado para o repositorio no Github através do push
+
+
+// BOAS PRÁTICAS
+
+//1 - Quase todos os comandos no git bash são feitos com letras minusculas
+//2 - Atenção com a sintaxe pois os erros sempre aparecerão em inglês, e poderá ser desafiador interpretá-los 
+//3 - Atenção à ordem dos comandos
